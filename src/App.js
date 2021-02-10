54df9dbda2f8ef14325281f91f7d8d4e487122d5
Robert Dupplaw <36577903+gcha9954@users.noreply.github.com>
2021-02-10 20:28:03 +0000

    stateful component configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import "./styles.css";
+import { Component } from "react";
+import marked from "marked";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: "",
+      preview: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      input: event.target.value,
+      preview: marked(event.target.value)
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h2>Input</h2>
+          <textarea name="editor" id="editor" onChange={this.handleChange}>
+            {this.state.input}
+          </textarea>
+        </div>
+        <div>
+          <h2>Preview</h2>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: this.state.preview }}
+          ></div>
+        </div>
+      </div>
+    );
+  }
 }
